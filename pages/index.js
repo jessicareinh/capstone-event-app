3b4eb17f52bb5f7813f04bf6f5b7e5dfdd05144e
jessicareinh <148791371+jessicareinh@users.noreply.github.com>
2024-02-02 16:27:17 +0100

    fetch event data

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,45 @@
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import Head from "next/head";
+//const apiKey = process.env.apiKey;
+
 export default function HomePage() {
+  const [data, setData] = useState([]);
+  const countryCode = "de";
+  const locale = "*";
+  const baseUrl = "https://app.ticketmaster.com/discovery/v2/events";
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(
+          "https://app.ticketmaster.com/discovery/v2/events?apikey=KUfDtCZdKiqgRrrY7R9qumcHOH8tg1RA"
+          //`${baseUrl}&${apiKey}&${countryCode}&${locale}`
+        );
+        if (!response.ok) {
+          throw new Error("failed to fetch api");
+        }
+        const json = await response.json();
+        setData(json._embedded.events);
+      } catch (error) {
+        console.log("Error");
+      }
+    }
+    fetchData();
+    console.log(data);
+  }, []);
+
   return (
-    <div>
-      <h1>Hello from Next.js</h1>
-    </div>
+    <>
+      <h1>sos</h1>
+      {data.map((event) => (
+        <div key={event.id}>
+          {event.name}
+          <img src={event.images[0].url} alt={event.name} />
+          {event.dates.start.localDate}
+          {event._embedded.venues[0].city.name}
+        </div>
+      ))}
+    </>
   );
 }
