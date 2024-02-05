ee182d343fc651d54e29273d3fa6884bc4a63e87
Thanh Tran <85113171+thanhtran-git@users.noreply.github.com>
2024-02-05 14:43:16 +0100

    add Styled component EventList

--- components/EventList/index.js
+++ components/EventList/index.js
@@ -1,20 +1,40 @@
 import EventCard from "../EventCard";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  max-width: 1400px;
+  justify-content: center;
+`;
+const StyledList = styled.li`
+  list-style: none;
+`;
+
+const CardContainer = styled.div`
+  display: flex;
+`;
 
 export default function EventList({ apiData }) {
   return (
     <>
-      {apiData.map((event) => (
-        <li key={event.id}>
-          <EventCard
-            title={event.name}
-            image={event.images[2].url}
-            date={event.dates.start.localDate}
-            location={event._embedded.venues[0].city.name}
-            width={300}
-            height={200}
-          />
-        </li>
-      ))}
+      <Wrapper>
+        {apiData.map((event) => (
+          <StyledList key={event.id}>
+            <CardContainer>
+              <EventCard
+                title={event.name}
+                location={event._embedded.venues[0].city.name}
+                venue={event._embedded.venues[0].name}
+                date={event.dates.start.localDate}
+                image={event.images[2].url}
+                width={300}
+                height={200}
+              />
+            </CardContainer>
+          </StyledList>
+        ))}
+      </Wrapper>
     </>
   );
 }
