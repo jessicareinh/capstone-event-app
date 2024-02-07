20fa000871401595366790951129e2948ef94064
Thanh Tran <85113171+thanhtran-git@users.noreply.github.com>
2024-02-07 16:02:43 +0100

    add card styling

--- components/EventCard/index.js
+++ components/EventCard/index.js
@@ -2,54 +2,66 @@ import Image from "next/image";
 import styled from "styled-components";
 import Link from "next/link";
 
-const Wrapper = styled.div`
-  width: 350px;
-  height: 380px;
-  margin: 20px;
-  padding: 20px;
-  border-radius: 5px;
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3);
+function formatDate(dateString) {
+  const date = new Date(dateString);
+  const day = date.getDate().toString().padStart(2, "0");
+  const month = (date.getMonth() + 1).toString().padStart(2, "0"); // Month starts from 0
+  const year = date.getFullYear();
+  return `${day}-${month}-${year}`;
+}
+
+const CardContainer = styled.div`
   display: flex;
   flex-direction: column;
+  width: 100%;
+  max-width: 370px;
+  height: 370px;
+  margin: 25px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3);
+  overflow: hidden;
+  &:hover {
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.8);
+  }
 `;
 
-const DetailsContainer = styled.div`
+const Title = styled.h3`
+  margin: 10px;
+`;
+
+const StyledImage = styled(Image)`
+  width: 100%;
+  height: 250px;
+  object-fit: cover;
+`;
+
+const DetailsRow = styled.div`
   display: flex;
   justify-content: space-between;
-  margin-top: 20px;
-`;
-
-const Title = styled.h2`
-  margin-top: 10px;
+  padding: 10px;
 `;
 
 export default function EventCard({
   title,
   image,
-  date,
+  formatDate,
   location,
-  venue,
   width,
   height,
   id,
 }) {
   return (
-    <>
-      <Wrapper>
-        <Link href={`/details/${id}`}>
-          <Image src={image} alt={title} width={width} height={height} />
-          <Title>{title}</Title>
-        </Link>
-        <DetailsContainer>
-          <div>
-            <p>{date}</p>
-          </div>
-          <div>
-            <p>{location}</p>
-            <p>{venue}</p>
-          </div>
-        </DetailsContainer>
-      </Wrapper>
-    </>
+    <CardContainer>
+      <Link href={`/details/${id}`}>
+        <StyledImage src={image} alt={title} width={width} height={height} />
+      </Link>
+
+      <Title>{title}</Title>
+
+      <DetailsRow>
+        <p>{location}</p>
+        <p>{formatDate}</p>
+      </DetailsRow>
+    </CardContainer>
   );
 }
