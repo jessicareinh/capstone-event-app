2681f0f743836faab59970eceee075fad5b63483
jessicareinh <148791371+jessicareinh@users.noreply.github.com>
2024-02-09 16:01:40 +0100

    display added event

--- pages/add-event/index.js
+++ pages/add-event/index.js
@@ -1,13 +1,29 @@
 import EventForm from "@/components/EventForm";
 import Link from "next/link";
-// import { uid } from "uid";
+import { useState } from "react";
+import { uid } from "uid";
 // import useLocalStorageState from "use-local-storage-state";
 
 export default function MyEvents({ onAddEvent }) {
+  const [myEvents, setMyEvents] = useState([]);
+  function handleAddEvent(newEvent) {
+    setMyEvents([...myEvents, { id: uid(), ...newEvent }]);
+  }
   return (
     <>
+      <EventForm onAddEvent={handleAddEvent} />
       <Link href="/my-events"> Back </Link>
-      <EventForm onAddEvent={onAddEvent} />
+      <ul>
+        {myEvents.map((event) => (
+          <li key={event.id}>
+            <p>{event.title}</p>
+            <p>{event.date}</p>
+            <p>{event.time}</p>
+            <p>{event.location}</p>
+            <p>{event.description}</p>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
