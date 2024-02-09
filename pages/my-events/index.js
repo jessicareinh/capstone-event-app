8fc1362ef024cad1c568f62f8aa6bd8ec74d170d
jessicareinh <148791371+jessicareinh@users.noreply.github.com>
2024-02-09 09:51:27 +0100

    add Events List Button

--- pages/my-events/index.js
+++ pages/my-events/index.js
@@ -1,7 +1,13 @@
+import Link from "next/link";
+
 export default function MyEvents() {
   return (
     <>
       <h1>My Events List</h1>
+
+      <Link href="/addEvent">
+        <button type="button">➕</button>
+      </Link>
     </>
   );
 }
