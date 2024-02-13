4f34909500951d0f5c96e0ad4cf6907e398da403
Edona Lleshi <edona.lleshi95@gmail.com>
2024-02-13 10:09:28 +0100

    add router

--- pages/add-event/index.js
+++ pages/add-event/index.js
@@ -1,21 +1,22 @@
 import EventForm from "@/components/EventForm";
 import Link from "next/link";
 import { uid } from "uid";
+import { useRouter } from "next/router";
 
 export default function MyEvents({ ownEvents, setOwnEvents }) {
+  const router = useRouter();
   function handleAddEvent(newEvent) {
     setOwnEvents([...ownEvents, { id: uid(), ...newEvent }]);
     alert("🎉You have added your Event successfully!");
+    router.push("/my-events");
   }
 
   return (
     <>
-
+      <h2>Add your own Event </h2>
       <EventForm onAddEvent={handleAddEvent} />
-      <Link href="/my-events"> MyEventList </Link>
-    <br/>
-      <Link href="/"> Back to homepage </Link>
 
+      <Link href="/"> Back to homepage </Link>
     </>
   );
 }
