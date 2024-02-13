a96051390a815bc8b577dec9b3163be5af2742c2
Edona Lleshi <edona.lleshi95@gmail.com>
2024-02-13 11:33:33 +0100

    add required

--- components/EventForm/index.js
+++ components/EventForm/index.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { useState } from "react";
+import styled from "styled-components";
 
 export default function EventForm({ onAddEvent }) {
   function handleSubmit(event) {
@@ -11,30 +12,31 @@ export default function EventForm({ onAddEvent }) {
     event.target.reset();
   }
 
+  const Paragraph = styled.p`
+    font-size: small;
+    color: gray;
+  `;
+
   return (
     <>
       <h2> Add your Own Event</h2>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="title">Title:</label>
+        <label htmlFor="title">Title*</label>
         <input type="text" id="title" name="title" required />
         <br></br>
-        <label htmlFor="date">Date:</label>
+        <label htmlFor="date">Date*</label>
         <input type="date" id="date" name="date" required />
-        <label htmlFor="time">Time:</label>
+        <label htmlFor="time">Time*</label>
         <input type="time" id="time" name="time" required />
         <br></br>
-        <label htmlFor="location">Location:</label>
+        <label htmlFor="location">Location*</label>
         <input type="text" id="location" name="location" required />
         <br></br>
-        <label htmlFor="description">Description:</label>
+        <label htmlFor="description">Description</label>
         <br></br>
-        <textarea
-          id="description"
-          name="description"
-          rows="8"
-          required
-        ></textarea>
+        <textarea id="description" name="description" rows="8"></textarea>
         <br></br>
+        <Paragraph>*: Required</Paragraph>
         <br></br>
 
         <button type="submit">Add Your Own Event</button>
