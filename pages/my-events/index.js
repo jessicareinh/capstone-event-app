bb34255f7f8b124312813f6d843c9559313915b5
Edona Lleshi <edona.lleshi95@gmail.com>
2024-02-14 16:37:18 +0100

    add handleDeleteEvent

--- pages/my-events/index.js
+++ pages/my-events/index.js
@@ -1,13 +1,22 @@
 import Link from "next/link";
 import MyEventsList from "@/components/MyEventsList";
-import styled from "styled-components";
+import useLocalStorageState from "use-local-storage-state";
 
 export default function MyEvents({ ownEvents }) {
+  const [storedOwnEvents, setStoredOwnEvents] = useLocalStorageState(
+    "myEvents",
+    ownEvents
+  );
+  const handleDeleteEvent = (eventId) => {
+    setStoredOwnEvents(storedOwnEvents.filter((event) => event.id !== eventId));
+  };
+
   return (
     <>
       <h1>My Events List</h1>
-      <MyEventsList ownEvents={ownEvents} />
+      <MyEventsList ownEvents={ownEvents} onDeleteEvent={handleDeleteEvent} />
       <Link href="/"> Back to homepage </Link>
+
       <br></br>
       <br></br>
       <Link href="/add-event">
