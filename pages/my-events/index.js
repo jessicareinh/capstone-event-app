a617574160d91e0ba02b62af20a3df3115496a0f
Thanh Tran <85113171+thanhtran-git@users.noreply.github.com>
2024-02-16 12:26:11 +0100

    add styling, links

--- pages/my-events/index.js
+++ pages/my-events/index.js
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import MyEventsList from "@/components/MyEventsList";
 import useLocalStorageState from "use-local-storage-state";
+import styled from "styled-components";
+import { useRouter } from "next/router";
 
 export default function MyEvents({ ownEvents }) {
   const [storedOwnEvents, setStoredOwnEvents] = useLocalStorageState(
@@ -8,23 +10,62 @@ export default function MyEvents({ ownEvents }) {
     ownEvents
   );
 
+  const router = useRouter();
+
+  const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  `;
+
+  const PageTitle = styled.h1`
+    text-align: center;
+    margin: 30px 0;
+  `;
+
+  const Nav = styled.nav`
+    display: flex;
+    gap: 30px;
+  `;
+
+  const NavLink = styled(Link)`
+    text-decoration: none;
+    color: navy;
+    &:hover {
+      text-decoration: underline;
+    }
+  `;
+
+  const Button = styled.button`
+    width: 120px;
+    height: 30px;
+    border-radius: 5px;
+    &:hover {
+      background-color: lightgray;
+    }
+  `;
   const handleDeleteEvent = (eventId) => {
     setStoredOwnEvents(storedOwnEvents.filter((event) => event.id !== eventId));
   };
 
   return (
     <>
-      <h1>My Events List</h1>
-      <MyEventsList
-        ownEvents={storedOwnEvents}
-        onDeleteEvent={handleDeleteEvent}
-      />
-      <Link href="/"> Back to homepage </Link>
-      <br></br>
-      <br></br>
-      <Link href="/add-event">
-        <button>Add your Event</button>
-      </Link>
+      <Wrapper>
+        <PageTitle>My Events List</PageTitle>
+        <Nav>
+          <NavLink href="/">Home</NavLink>
+          <NavLink href="/favorites">Favorites</NavLink>
+        </Nav>
+
+        <MyEventsList
+          ownEvents={storedOwnEvents}
+          onDeleteEvent={handleDeleteEvent}
+        />
+
+        <Button type="button" onClick={() => router.push("/add-event")}>
+          Add your Event
+        </Button>
+      </Wrapper>
     </>
   );
 }
