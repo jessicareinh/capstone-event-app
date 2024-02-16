08724b57b148282cc5d391bb66bfe8fc8b8397bd
jessicareinh <148791371+jessicareinh@users.noreply.github.com>
2024-02-16 12:46:18 +0100

    Refactor Edit in Component

--- components/MyEventsList/index.js
+++ components/MyEventsList/index.js
@@ -1,57 +1,10 @@
 import styled from "styled-components";
 import { useState } from "react";
+import EditEvent from "../EditEvent";
 
 const StyledList = styled.li`
   list-style: none;
   margin: 30px;
-`;
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  max-width: 1300px;
-`;
-
-const Paragraph = styled.p`
-  margin: 10px;
-`;
-
-const EventCard = styled.li`
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3);
-  background-color: lightpink;
-  margin-top: 40px;
-  border-radius: 10px;
-`;
-
-const Title = styled.h2`
-  font-weight: 600;
-`;
-
-const Section = styled.section`
-  font-family: Inter, sans-serif;
-  font-size: 12px;
-  color: #111;
-`;
-
-const StyledEditButton = styled.button`
-  align-items: center;
-  background-color: #fffbf5;
-  border: 2px solid #111;
-  border-radius: 8px;
-  box-sizing: border-box;
-  color: #111;
-  cursor: pointer;
-  display: flex;
-  font-family: Inter, sans-serif;
-  font-size: 16px;
-  height: 30px;
-  justify-content: center;
-  line-height: 24px;
-  max-width: 100%;
-  padding: 0 10px;
-  text-align: center;
-  text-decoration: none;
 `;
 
 export default function MyEventsList({ ownEvents }) {
@@ -77,61 +30,7 @@ export default function MyEventsList({ ownEvents }) {
     <StyledList>
       {ownEvents.length === 0 && <p>You have not added any events yet </p>}
       {ownEvents.map((event) => (
-        <EventCard key={event.id}>
-          <Title>{event.title}</Title>
-          <Paragraph>{event.date}</Paragraph>
-          <Paragraph>{event.time}</Paragraph>
-          <Paragraph>{event.location}</Paragraph>
-          <Paragraph>{event.description}</Paragraph>
-          {isEditing && editEvent === event.id && (
-            <Section>
-              <input
-                type="text"
-                defaultValue={event.title}
-                onChange={(e) => (event.title = e.target.value)}
-                placeholder="Title*"
-                required
-              />
-
-              <input
-                htmlFor="date"
-                defaultValue={event.date}
-                onChange={(e) => (event.date = e.target.value)}
-                placeholder="Date"
-                required
-              />
-
-              <input
-                htmlFor="time"
-                defaultValue={event.time}
-                onChange={(e) => (event.time = e.target.value)}
-                placeholder="Time"
-                required
-              />
-
-              <input
-                type="text"
-                defaultValue={event.location}
-                onChange={(e) => (event.location = e.target.value)}
-                placeholder="Location"
-                required
-              />
-              <input
-                type="text"
-                defaultValue={event.description}
-                onChange={(e) => (event.description = e.target.value)}
-                placeholder="Description"
-              />
-              <button onClick={() => handleSave(event.id)}>Save</button>
-              <button onClick={handleCancel}>Cancel</button>
-            </Section>
-          )}
-          {!isEditing && (
-            <StyledEditButton onClick={() => handleEdit(event.id)}>
-              edit
-            </StyledEditButton>
-          )}
-        </EventCard>
+        <EditEvent keys={event.id} event={event} onSave={handleSave} />
       ))}
     </StyledList>
   );
