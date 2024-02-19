f7e2c52f16077c5901c2ed560089684fad9bfbdf
Thanh Tran <85113171+thanhtran-git@users.noreply.github.com>
2024-02-19 09:42:38 +0100

    add date format function

--- components/utils/index.js
+++ components/utils/index.js
@@ -1,10 +1,23 @@
-export default function selectImage(images) {
+//function to select image from API based on condition
+export function selectImage(condition, images) {
   let selectedImage = null;
 
   for (let i = 0; i < images.length; i++) {
-    if (images[i].url.includes("RETINA_PORTRAIT_16_9")) {
+    if (images[i].url.includes(condition)) {
       selectedImage = images[i];
       return selectedImage;
     }
   }
+}
+
+//function to format date to German format
+export function formatDate(apiDate) {
+  const date = new Date(apiDate);
+  const options = {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+  };
+  const dateFormatter = new Intl.DateTimeFormat("de-DE", options);
+  return dateFormatter.format(date);
 }
