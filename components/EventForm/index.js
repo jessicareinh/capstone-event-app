81eba17e45c9de7b4b182d504c2979d129245791
jessicareinh <148791371+jessicareinh@users.noreply.github.com>
2024-02-20 16:50:44 +0100

    edit Input Form

--- components/EventForm/index.js
+++ components/EventForm/index.js
@@ -1,11 +1,61 @@
-import Link from "next/link";
-import { useState } from "react";
 import styled from "styled-components";
 import { useRouter } from "next/router";
 
+const H2 = styled.h2`
+  font-family: monospace;
+  margin-top: 30px;
+  font-size: 32px;
+`;
 const Paragraph = styled.p`
   font-size: small;
   color: gray;
+`;
+
+const Input = styled.input`
+  font-family: monospace;
+  border-radius: 6px;
+  outline: 3px;
+  height: 35px;
+  width: 300px;
+  border: 0px;
+  background-color: #d1c4e9;
+  padding: 5px;
+
+  &:focus {
+    background-color: #cbdccb;
+  }
+`;
+
+const Textarea = styled.textarea`
+  font-family: monospace;
+  border-radius: 8px;
+  background-color: #d1c4e9;
+  border: 0;
+  padding: 5px;
+
+  &:focus {
+    background-color: #cbdccb;
+  }
+`;
+const AddButton = styled.button`
+  border-radius: 12px;
+  border: 0;
+  background-color: #bdbdbd;
+  font-family: monospace;
+  height: 40px;
+  &:hover {
+    background-color: #9e9e9e;
+  }
+`;
+
+const Form = styled.form`
+  display: grid;
+  border-radius: 20px;
+  box-sizing: border-box;
+  height: 500px;
+  padding: 20px;
+  width: 320px;
+  font-family: monospace;
 `;
 
 export default function EventForm({ onAddEvent }) {
@@ -22,28 +72,28 @@ export default function EventForm({ onAddEvent }) {
 
   return (
     <>
-      <h2> Add your Own Event</h2>
-      <form onSubmit={handleSubmit}>
+      <H2> Add your Own Event</H2>
+      <Form onSubmit={handleSubmit}>
         <label htmlFor="title">Title*</label>
-        <input type="text" id="title" name="title" required />
+        <Input type="text" id="title" name="title" required />
         <br></br>
         <label htmlFor="date">Date*</label>
-        <input type="date" id="date" name="date" required />
+        <Input type="date" id="date" name="date" required />
         <label htmlFor="time">Time*</label>
-        <input type="time" id="time" name="time" required />
+        <Input type="time" id="time" name="time" required />
         <br></br>
         <label htmlFor="location">Location*</label>
-        <input type="text" id="location" name="location" required />
+        <Input type="text" id="location" name="location" required />
         <br></br>
         <label htmlFor="description">Description</label>
         <br></br>
-        <textarea id="description" name="description" rows="8"></textarea>
+        <Textarea id="description" name="description" rows="8"></Textarea>
         <br></br>
         <Paragraph>*: Required</Paragraph>
         <br></br>
 
-        <button type="submit">Add Your Own Event</button>
-      </form>
+        <AddButton type="submit">Add Your Own Event</AddButton>
+      </Form>
     </>
   );
 }
