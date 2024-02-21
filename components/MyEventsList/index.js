d3622fc160c86460b15bdb89b4d2a1df9cc6851e
jessicareinh <148791371+jessicareinh@users.noreply.github.com>
2024-02-21 12:00:05 +0100

    fix add event Button

--- components/MyEventsList/index.js
+++ components/MyEventsList/index.js
@@ -1,49 +1,103 @@
 import styled from "styled-components";
 import DeleteButton from "../DeleteButton";
 import EditEvent from "../EditEvent";
+import Link from "next/link";
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`;
 
 const StyledList = styled.ul`
   list-style: none;
   line-height: 2rem;
 `;
 
+const P = styled.p`
+  margin-top: 40px;
+  font-family: monospace;
+`;
+
 const EventCard = styled.div`
   box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.7);
   background-color: #d1c4e9;
-  margin: 30px;
+  margin: 50px;
   padding: 15px;
   width: 250px;
-  height: 400px;
+  min-height: 200px;
   border-radius: 10px;
 `;
 
+const AddEventButton = styled.button`
+  border-radius: 8px;
+  border: 5px;
+  box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.7);
+  background-color: #bdbdbd;
+  font-family: monospace;
+  height: 25px;
+  margin: 15px;
+  padding: 3px;
+  cursor: poninter;
+  width: 250px;
+
+  &:hover {
+    background-color: #9e9e9e;
+  }
+`;
+
+const ButtonContainer = styled.div`
+  text-align: center;
+`;
+const H1 = styled.h1`
+  margin-top: 30px;
+  font-family: monospace;
+  font-size: 32px;
+  font-weight: bold;
+`;
 const Title = styled.h2`
   font-weight: 600;
 `;
 
 export default function MyEventsList({ ownEvents, onDeleteEvent, onSave }) {
   if (!ownEvents || ownEvents.length === 0) {
-    return <p>You have not added any events yet </p>;
+    return (
+      <>
+        <P>You have not added any events yet... </P>
+        <Link href="/add-event">
+          <AddEventButton>Add your Event</AddEventButton>
+        </Link>
+      </>
+    );
   }
   return (
     <>
-      {ownEvents.map((event) => (
-        <EventCard key={event.id}>
-          <Title>{event.title}</Title>
-          <StyledList>
-            <li>{event.date}</li>
-            <li>{event.time}</li>
-            <li>{event.location}</li>
-            <li>{event.description}</li>
-          </StyledList>
-          <EditEvent key={event.id} event={event} onSave={onSave} />
-          <DeleteButton
-            id={event.id}
-            onDeleteEvent={() => onDeleteEvent(event.id)}
-            confirmMessage="Are you sure you want to delete this event?"
-          />
-        </EventCard>
-      ))}
+      <Wrapper>
+        <H1>My Events List</H1>
+        {ownEvents.map((event) => (
+          <EventCard key={event.id}>
+            <Title>{event.title}</Title>
+            <StyledList>
+              <li>{event.date}</li>
+              <li>{event.time}</li>
+              <li>{event.location}</li>
+              <li>{event.description}</li>
+            </StyledList>
+            <ButtonContainer>
+              <EditEvent key={event.id} event={event} onSave={onSave} />
+              <DeleteButton
+                id={event.id}
+                onDeleteEvent={() => onDeleteEvent(event.id)}
+                confirmMessage="Are you sure you want to delete this event?"
+              />
+            </ButtonContainer>
+          </EventCard>
+        ))}
+        <Link href="/add-event">
+          <AddEventButton>Add your Event</AddEventButton>
+        </Link>
+      </Wrapper>
     </>
   );
 }
