d0cd01a968631614e61d354c1b5cce533ce24381
jessicareinh <148791371+jessicareinh@users.noreply.github.com>
2024-02-21 12:00:26 +0100

    fix page

--- pages/my-events/index.js
+++ pages/my-events/index.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import MyEventsList from "@/components/MyEventsList";
 import useLocalStorageState from "use-local-storage-state";
+import styled from "styled-components";
 
 export default function MyEvents({ ownEvents, onSave }) {
   const [storedOwnEvents, setStoredOwnEvents] = useLocalStorageState(
@@ -14,18 +15,11 @@ export default function MyEvents({ ownEvents, onSave }) {
 
   return (
     <>
-      <h1>My Events List</h1>
       <MyEventsList
         ownEvents={storedOwnEvents}
         onDeleteEvent={handleDeleteEvent}
         onSave={onSave}
       />
-      <Link href="/"> Back to homepage </Link>
-      <br></br>
-      <br></br>
-      <Link href="/add-event">
-        <button>Add your Event</button>
-      </Link>
     </>
   );
 }
