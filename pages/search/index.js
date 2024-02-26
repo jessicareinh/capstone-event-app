aa73133e3852ca47df1f71e5f1b0ba31784c90d0
Thanh Tran <85113171+thanhtran-git@users.noreply.github.com>
2024-02-26 10:02:32 +0100

    move styled comps back

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,6 +1,50 @@
 import React, { useState } from "react";
 import EventList from "@/components/EventList";
-import { Wrapper, Form, Input, Button, ResultAmount } from "./styles";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Form = styled.form`
+  display: flex;
+  margin: 20px;
+`;
+
+const Input = styled.input`
+  padding: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  font-size: 16px;
+  flex-grow: 1;
+  margin-right: 1rem;
+
+  &:focus {
+    outline: none;
+    border-color: #999;
+  }
+`;
+
+const Button = styled.button`
+  padding: 1rem 1.5rem;
+  background-color: #333;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  font-size: 16px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #222;
+  }
+`;
+
+const ResultAmount = styled.div`
+  margin-top: 1rem;
+  text-align: center;
+`;
 
 export default function Search({
   searchData,
