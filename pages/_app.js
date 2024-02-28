701305f78f6034b504c16a007e36fea2dcbe0fa8
Thanh Tran <85113171+thanhtran-git@users.noreply.github.com>
2024-02-28 10:45:46 +0100

    add unified state, fetch for category buttons

--- pages/_app.js
+++ pages/_app.js
@@ -6,11 +6,13 @@ import Layout from "@/components/Layout";
 import fetchData from "@/components/FetchApi";
 import LoadingAnimation from "@/components/LoadingAnimation";
 
-
 export default function App({ Component, pageProps }) {
   const [apiData, setApiData] = useState([]);
   const [searchData, setSearchData] = useState([]);
+  const [categoryEvents, setCategoryEvents] = useState([]);
   const [page, setPage] = useState(1);
+
+  const [city, setCity] = useState("");
   const [loading, setLoading] = useState(true);
 
   const [ownEvents, setOwnEvents] = useLocalStorageState("myEvents", {
@@ -19,23 +21,48 @@ export default function App({ Component, pageProps }) {
   const [favList, setFavList] = useLocalStorageState("favList", {
     defaultValue: [],
   });
+  //combines apiData, searchData and categoryEvents to one unified state for pages/[id] and /favorites
+  const [combinedData, setCombinedData] = useLocalStorageState("combinedData", {
+    defaultValue: [],
+  });
 
+  //fetches data on homepage
   useEffect(() => {
-    fetchData(`page=${page}&size=27`, (data) =>
-      setApiData((prev) => [...prev, ...data._embedded.events])
-    );
-  }, [page]);
+    fetchData(`page=${page}&city=${city}&size=27`, (apiData) => {
+      setApiData(apiData._embedded.events);
+      setCombinedData([...combinedData, ...apiData._embedded.events]);
+    });
+  }, [page, city]);
+  {
+    /*please check if 2 depenencies are avoidable*/
+  }
 
   //triggers search on submit in /search
-  async function handleSearch(query) {
-    fetchData(`keyword=${query}&size=50`, (searchData) =>
-      setSearchData(searchData._embedded.events)
+  function handleSearch(query) {
+    fetchData(`keyword=${query}&size=50`, (searchData) => {
+      setSearchData(searchData._embedded.events);
+      setCombinedData([...combinedData, ...searchData._embedded.events]);
+    });
+  }
+  //fetches events based on clicked category button
+  function handleAddCategory(category) {
+    fetchData(
+      `classificationName=${category}&city=${city}&size=54`,
+      (categoryEvents) => {
+        setCategoryEvents(categoryEvents._embedded.events);
+        setCombinedData([...combinedData, ...categoryEvents._embedded.events]);
+      }
     );
+  }
+  //sets the selected city from dropdownmenu as state for both homepage and categories
+  function handleCityChange(city) {
+    setCity(city);
   }
 
   //loads the next page of event cards in the homepage "/"
   function handleLoadMore() {
     setPage(page + 1);
+    window.scrollTo({ top: 0, behavior: "smooth" });
   }
 
   //CRUD functions
@@ -79,8 +106,13 @@ export default function App({ Component, pageProps }) {
           />
         ) : null}
         <Component
+          combinedData={combinedData}
           apiData={apiData}
           searchData={searchData}
+          categoryEvents={categoryEvents}
+          handleAddCategory={handleAddCategory}
+          city={city}
+          onCityChange={handleCityChange}
           ownEvents={ownEvents}
           favList={favList}
           onSubmit={handleSearch}
