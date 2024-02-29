55c80407ee885b07bf0850da24a6f3f6832ef4cc
Edona Lleshi <edona.lleshi95@gmail.com>
2024-02-29 10:04:00 +0100

    add title

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,8 +1,12 @@
+import Head from "next/head";
 import Navigation from "../Navigation";
 
 export default function Layout({ children }) {
   return (
     <>
+      <Head>
+        <title>My Events</title>
+      </Head>
       <header>
         <Navigation />
         <main>{children}</main>
